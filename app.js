0c4feed3af18908834a75894ad46d86a58af6fbd
Difelice-e <difelice.emanuele@gmail.com>
2022-03-05 15:15:40 +0100

    esercizio completato

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-let app = new Vue(
-    {
-        el: '#root',
-        data: {}, 
-        methods: {}
-    }
-)
+const app = new Vue({
+    el: '#app',
+    data:{
+      eMail: [],
+    },
+    created() {
+  
+      for (let i = 0; i < 10; i++){
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((res) =>{
+            this.eMail.push(res.data.response);
+          })
+      }
+    
+    },
+  })
